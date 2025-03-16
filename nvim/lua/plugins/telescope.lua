38e0793b61b67d2b0d7d123fa28dd354582456ef
ag <ag@gmail.com>
2025-03-16 19:05:31 +0530

    update

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -2,8 +2,16 @@ return {
     'nvim-telescope/telescope.nvim', tag = '0.1.8',
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
+        require 'telescope'.setup {
+            defaults = {
+                file_ignore_patterns = {
+                    "node_modules"
+                }
+            }
+        }
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>f', builtin.find_files, { desc = 'Telescope find files' })
         vim.keymap.set('n', '<leader>g', builtin.live_grep, { desc = 'Telescope live grep' })
     end
 }
+
